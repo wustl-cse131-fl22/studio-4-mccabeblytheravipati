dbff25cd887e0a09340f49a3617319a85598fcea
kate <kate@katherines-mbp>
2022-10-12 17:12:56 -0500

    All Done!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,40 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		//quadrant bagel
+		StdDraw.setPenColor(40,179,242);
+		StdDraw.filledRectangle(0.25,0.25, 0.25, 0.25); 
+		StdDraw.setPenColor(255,207,144);
+		StdDraw.filledCircle(0.25,0.25,0.25);
+		StdDraw.setPenColor(40,179,242);
+		StdDraw.filledCircle(0.25, 0.25, 0.05);
+		
+		//quadrant star
+		StdDraw.setPenColor(255,3,255);
+		StdDraw.filledRectangle(0.75,0.25,0.25,0.25);
+		StdDraw.setPenColor(Color.YELLOW);
+		double x [] = {.6,.75,.9};
+		double y [] = {.1,.4,.1};
+		StdDraw.filledPolygon(x,y);
+		double x2 [] = {.6,.75,.9};
+		double y2 [] = {.3,.05,.3};
+		StdDraw.filledPolygon(x2,y2);
+		
+		//quadrant equal
+		StdDraw.setPenColor(68,185,72);
+		StdDraw.filledRectangle(0.25,0.75,0.25,0.25);
+		StdDraw.setPenColor(62,87,129);
+		StdDraw.filledRectangle(0.25,0.68,0.15,0.05);
+		StdDraw.filledRectangle(0.25,0.82,0.15,0.05);
+		
+		//quadrant heart
+		StdDraw.setPenColor(248,134,72);
+		StdDraw.filledRectangle(0.75,0.75,0.25,0.25);
+		StdDraw.setPenColor(255,153,187);
+		StdDraw.filledCircle(0.66,0.8,0.09);
+		StdDraw.filledCircle(0.84,0.8,0.09);
+		double x3 [] = {.58,.92,0.75};
+		double y3 [] = {.8,.8,0.55};
+		StdDraw.filledPolygon(x3,y3);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,54 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double one = in.nextDouble();
+		double two = in.nextDouble();
+		double three = in.nextDouble();
+		double four = in.nextDouble();
+		//now all the entries are saved variables
+		
+		StdDraw.setPenColor(red,green,blue); //set the pen color
+		
+		if (shape.equals("rectangle")){
+			if (filled==true) {
+			StdDraw.filledRectangle(one,two,three,four);
+			}
+			else {
+			StdDraw.rectangle(one,two,three,four);
+			}
+		}
+		//rectangle
+			
+		if (shape.equals("ellipse")) {
+			if (filled==true) {
+			StdDraw.filledEllipse(one, two, three, four);	
+			}
+			else {
+			StdDraw.ellipse(one, two, three, four);	
+			}
+		}
+		//ellipse
+		
+		if (shape.equals("triangle")) {
+			double five = in.nextDouble();
+			double six = in.nextDouble();
+			double doublex [] = {one,three,five};
+			double doubley [] = {two,four,six};
+			if (filled==true) {
+			StdDraw.filledPolygon(doublex,doubley);	
+			}
+			else {
+			StdDraw.polygon(doublex,doubley);	
+			}
+				
+		}
+			
 	}
+		
 }
+
